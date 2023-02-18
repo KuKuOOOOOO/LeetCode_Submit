13e88f3712d7151d61578f74bea28660ac465531
bubble_chang <bubble_chang@protech.com.tw>
2023-02-18 17:48:24 +0800

    Update: Add Easy 27

---
+++ 27.remove-element.cs
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=27 lang=csharp
+ *
+ * [27] Remove Element
+ */
+
+// @lc code=start
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int cnt = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] != val)
+            {
+                nums[cnt++] = nums[i];
+            }
+        }
+        return cnt;
+    }
+}
+// @lc code=end
