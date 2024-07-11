4ca1a7402f3d033879b7e0c8b97e3d9ecd2d34b0
Fate <a981211661@gmail.com>
2024-07-12 00:08:45 +0800

    Update: 1. Add Easy 104

---
+++ 104.maximum-depth-of-binary-tree.cs
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=104 lang=csharp
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public int MaxDepth(TreeNode root)
+    {
+        if (root == null)
+            return 0;
+
+        return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+    }
+}
+// @lc code=end
+
